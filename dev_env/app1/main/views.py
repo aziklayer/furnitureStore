35aac6000ceaeb09bd1bd274520dc2c7be5e46df
Azik <azik8423@gmail.com>
2024-03-07 17:27:52 +0500

    приложение main, страницы index и about

---
+++ dev_env/app1/main/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page':'Текст о том почему этот сайт такой классный.',
+    }
+    return render(request, 'main/about.html', context)
