83ffeb961befec320dbfaede6fb5621afa8b9fe2
Azik <azik8423@gmail.com>
2024-03-16 15:38:44 +0500

    до JS Ajax

--- dev_env/app1/app/settings.py
+++ dev_env/app1/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'main',
     'goods',
     'users',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,4 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
