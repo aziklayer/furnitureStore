36c8269ded6dca62094861eec5f53fd72f8204fa
Azik <azik8423@gmail.com>
2024-03-17 14:09:56 +0500

    Final

--- dev_env/app1/users/admin.py
+++ dev_env/app1/users/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from users.models import User
 
-admin.site.register(User)
+from carts.admin import CartTabAdmin
+
+from orders.admin import OrderTabulareAdmin
+
+
+#admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "first_name", "last_name", "email",]
+    search_fields = ["username", "first_name", "last_name", "email",]
+
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
 
 
